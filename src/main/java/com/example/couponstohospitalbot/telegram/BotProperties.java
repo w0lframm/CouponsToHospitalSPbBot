cfc23e05069064d10a494e03fd617e9d356db1ef
Ekaterina Borisova <kate_boriso2002@mail.ru>
2022-09-23 14:19:16 +0300

    Add BotProperties.java

---
+++ src/main/java/com/example/couponstohospitalbot/telegram/BotProperties.java
@@ -0,0 +1,16 @@
+package com.example.couponstohospitalbot.telegram;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Getter
+@Setter
+@Configuration
+@ConfigurationProperties(prefix = "bot")
+public class BotProperties {
+    private String name;
+    private String token;
+}
+
